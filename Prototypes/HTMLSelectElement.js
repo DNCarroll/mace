a818e16d12309bb40eba7ac530b3f25c93c86a1c
Nathan Carroll <ncarroll@boongroup.com>
2016-12-31 10:18:38 -0600

    HTMLSelectElement minified.  Web site worked.

--- Prototypes/HTMLSelectElement.js
+++ Prototypes/HTMLSelectElement.js
@@ -1,32 +1,35 @@
 HTMLSelectElement.prototype.AddOptions = function (arrayOrObject, valueProperty, displayProperty, selectedValue) {
-    var select = this;
-    var addOption = function (display, value) {
-        var option = new Option(display, value);
-        select["options"][select.options.length] = option;
-        if (selectedValue && value === selectedValue) {
-            option.selected = true;
+    var s = this;
+    var aoo = arrayOrObject;
+    var ao = function (d, v) {
+        var o = new Option(d, v);
+        s["options"][s.options.length] = o;
+        if (selectedValue && v === selectedValue) {
+            o.selected = true;
         }
     };
-    if (Is.Array(arrayOrObject)) {
-        var tempArray = arrayOrObject;
-        if (displayProperty && valueProperty) {
-            tempArray.forEach(function (t) {
-                addOption(t[displayProperty], t[valueProperty]);
+    if (Is.Array(aoo)) {
+        var ta = aoo;
+        var dp = displayProperty;
+        var vp = valueProperty;
+        if (dp && vp) {
+            ta.forEach(function (t) {
+                ao(t[dp], t[vp]);
             });
         }
-        else if (tempArray.length > 1 && typeof tempArray[0] === 'string') {
-            tempArray.forEach(function (t) {
-                addOption(t, t);
+        else if (ta.length > 1 && typeof ta[0] === 'string') {
+            ta.forEach(function (t) {
+                ao(t, t);
             });
         }
     }
-    else if (arrayOrObject) {
-        for (var prop in arrayOrObject) {
-            if (!(arrayOrObject[prop] && {}.toString.call(arrayOrObject[prop]) === '[object Function]')) {
-                addOption(arrayOrObject[prop], arrayOrObject[prop]);
+    else if (aoo) {
+        for (var p in aoo) {
+            if (!(aoo[p] && {}.toString.call(aoo[p]) === '[object Function]')) {
+                ao(aoo[p], aoo[p]);
             }
         }
     }
-    return select;
+    return s;
 };
 //# sourceMappingURL=HTMLSelectElement.js.map
--- Prototypes/HTMLSelectElement.ts
+++ Prototypes/HTMLSelectElement.ts
@@ -2,33 +2,36 @@ interface HTMLSelectElement {
     AddOptions(arrayOrObject, valueProperty?: string, displayProperty?: string, selectedValue?): HTMLSelectElement;    
 }
 HTMLSelectElement.prototype.AddOptions= function(arrayOrObject, valueProperty ? : string, displayProperty?: string, selectedValue?): HTMLSelectElement {
-    var select = <HTMLSelectElement>this;
-    var addOption = (display, value) => {
-        var option = new Option(display, value);
-        select["options"][select.options.length] = option;
-        if (selectedValue && value === selectedValue) {
-            option.selected = true;
+    var s = <HTMLSelectElement>this;
+    var aoo = arrayOrObject;
+    var ao = (d, v) => {
+        var o = new Option(d, v);
+        s["options"][s.options.length] = o;
+        if (selectedValue && v === selectedValue) {
+            o.selected = true;
         }
     };
-    if (Is.Array(arrayOrObject)) {
-        var tempArray = <Array<any>>arrayOrObject;
-        if (displayProperty && valueProperty) {
-            tempArray.forEach(t => {  
-                addOption(t[displayProperty], t[valueProperty]);              
+    if (Is.Array(aoo)) {
+        var ta = <Array<any>>aoo;
+        var dp = displayProperty;
+        var vp = valueProperty;
+        if (dp && vp) {
+            ta.forEach(t => {  
+                ao(t[dp], t[vp]);              
             });                        
         }
-        else if (tempArray.length > 1 && typeof tempArray[0] === 'string') {
-            tempArray.forEach(t => {
-                addOption(t, t);
+        else if (ta.length > 1 && typeof ta[0] === 'string') {
+            ta.forEach(t => {
+                ao(t, t);
             });
         }
     }
-    else if (arrayOrObject) {        
-        for (var prop in arrayOrObject) {
-            if (!(arrayOrObject[prop] && {}.toString.call(arrayOrObject[prop]) === '[object Function]')) {
-                addOption(arrayOrObject[prop], arrayOrObject[prop]);                
+    else if (aoo) {        
+        for (var p in aoo) {
+            if (!(aoo[p] && {}.toString.call(aoo[p]) === '[object Function]')) {
+                ao(aoo[p], aoo[p]);                
             }
         }
     }    
-    return select;
+    return s;
 };
