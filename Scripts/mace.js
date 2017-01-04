4b8b81ca3d2bbcadb31431dc0b5b840ce5fbd0b2
Nathan Carroll <ncarroll@boongroup.com>
2017-01-03 21:55:13 -0600

    Unit test minification added brings size down some.

--- Scripts/mace.js
+++ Scripts/mace.js
@@ -926,80 +926,6 @@ var HistoryContainer;
 })(HistoryContainer || (HistoryContainer = {}));
 var HistoryManager = new HistoryContainer.History();
 //# sourceMappingURL=HistoryManager.js.map
-var Initializer;
-(function (Initializer) {
-    function WindowLoad(e) {
-        var w = window;
-        if (document.readyState === "complete") {
-            windowLoaded();
-        }
-        else {
-            if (w.onload) {
-                var c = w.onload, nl = function () {
-                    c(e);
-                    windowLoaded();
-                };
-                w.onload = nl;
-            }
-            else {
-                w.onload = function () {
-                    windowLoaded();
-                };
-            }
-        }
-    }
-    Initializer.WindowLoad = WindowLoad;
-    function windowLoaded() {
-        var w = window;
-        addViewContainers();
-        setProgressElement();
-        w.ShowByUrl(w.location.pathname.substring(1));
-        w.addEventListener("popstate", HistoryManager.BackEvent);
-    }
-    function addViewContainers() {
-        var it = ignoreTheseNames(), w = window;
-        for (var o in w) {
-            var n = getNameToTest(getStringOf(w[o]), it);
-            if (!Is.NullOrEmpty(n)) {
-                try {
-                    var no = (new Function("return new " + n + "();"))();
-                    if (Has.Properties(no, "IsDefault", "Views", "Show", "Url", "UrlPattern", "UrlTitle", "IsUrlPatternMatch")) {
-                        ViewContainers.Add(no);
-                    }
-                }
-                catch (e) {
-                    w.Exception(e);
-                }
-            }
-        }
-    }
-    function getNameToTest(rawFunction, ignoreThese) {
-        var rf = rawFunction;
-        if (!Is.NullOrEmpty(rf)) {
-            var p = "^function\\s(\\w+)\\(\\)", m = rf.match(p);
-            if (m && !ignoreThese.First(function (i) { return i === m[1]; })) {
-                return m[1];
-            }
-        }
-        return null;
-    }
-    function getStringOf(o) {
-        return o && o.toString ? o.toString() : null;
-    }
-    function setProgressElement() {
-        var pg = document.getElementById("progress");
-        if (pg != null) {
-            ProgressManager.ProgressElement = pg;
-        }
-    }
-    function ignoreTheseNames() {
-        return ["Ajax", "Binder", "DataObject", "View", "ViewContainer", "ViewContainers",
-            "ViewInstance", "EventType", "CustomEventArg", "Listener", "PropertyListener",
-            "ObjectState", "HistoryManager", "Initializer", "Is", "ProgressManager"];
-    }
-})(Initializer || (Initializer = {}));
-Initializer.WindowLoad();
-//# sourceMappingURL=Initializer.js.map
 var Is;
 (function (Is) {
     function Array(value) {
@@ -1327,3 +1253,77 @@ Window.prototype.ShowByUrl = function (url) {
     }
 };
 //# sourceMappingURL=Window.js.map
+var Initializer;
+(function (Initializer) {
+    function WindowLoad(e) {
+        var w = window;
+        if (document.readyState === "complete") {
+            windowLoaded();
+        }
+        else {
+            if (w.onload) {
+                var c = w.onload, nl = function () {
+                    c(e);
+                    windowLoaded();
+                };
+                w.onload = nl;
+            }
+            else {
+                w.onload = function () {
+                    windowLoaded();
+                };
+            }
+        }
+    }
+    Initializer.WindowLoad = WindowLoad;
+    function windowLoaded() {
+        var w = window;
+        addViewContainers();
+        setProgressElement();
+        w.ShowByUrl(w.location.pathname.substring(1));
+        w.addEventListener("popstate", HistoryManager.BackEvent);
+    }
+    function addViewContainers() {
+        var it = ignoreTheseNames(), w = window;
+        for (var o in w) {
+            var n = getNameToTest(getStringOf(w[o]), it);
+            if (!Is.NullOrEmpty(n)) {
+                try {
+                    var no = (new Function("return new " + n + "();"))();
+                    if (Has.Properties(no, "IsDefault", "Views", "Show", "Url", "UrlPattern", "UrlTitle", "IsUrlPatternMatch")) {
+                        ViewContainers.Add(no);
+                    }
+                }
+                catch (e) {
+                    w.Exception(e);
+                }
+            }
+        }
+    }
+    function getNameToTest(rawFunction, ignoreThese) {
+        var rf = rawFunction;
+        if (!Is.NullOrEmpty(rf)) {
+            var p = "^function\\s(\\w+)\\(\\)", m = rf.match(p);
+            if (m && !ignoreThese.First(function (i) { return i === m[1]; })) {
+                return m[1];
+            }
+        }
+        return null;
+    }
+    function getStringOf(o) {
+        return o && o.toString ? o.toString() : null;
+    }
+    function setProgressElement() {
+        var pg = document.getElementById("progress");
+        if (pg != null) {
+            ProgressManager.ProgressElement = pg;
+        }
+    }
+    function ignoreTheseNames() {
+        return ["Ajax", "Binder", "DataObject", "View", "ViewContainer", "ViewContainers",
+            "ViewInstance", "EventType", "CustomEventArg", "Listener", "PropertyListener",
+            "ObjectState", "HistoryManager", "Initializer", "Is", "ProgressManager"];
+    }
+})(Initializer || (Initializer = {}));
+Initializer.WindowLoad();
+//# sourceMappingURL=Initializer.js.map
