945e15a4de8507711cea3649d9915031ef11ce39
Nathan Carroll <omnisoldat@live.com>
2017-07-10 09:59:34 -0500

    Bower adjustment.

--- mace.Test/UnitTest1.cs
+++ mace.Test/UnitTest1.cs
@@ -6,42 +6,47 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace mace.Test {
     [TestClass]
     public class SimpleMinification {
+
         [TestMethod]
-        public void MakeMinifiedMaceJs() {
-            writeMaceJsFile();
-            var path = Path.Combine(pathToMaceWeb(), "Scripts");
-            var file = Path.Combine(path, "mace.js");
-            var contents = System.IO.File.ReadAllText(file);
+        public void MakeBowerFiles() {
+            var jsFile = writeBundle(".js");
+            var tsFile = writeBundle(".ts");
+            var contents = System.IO.File.ReadAllText(jsFile);
             var pattern = @"\r\n|\t|\s\s";
             var comments = @"//.*?\r\n";
             var min = System.Text.RegularExpressions.Regex.Replace(contents, comments, "");
-            var newFilePath = Path.Combine(path, "mace.min.js");
-
+            var newFilePath = Path.Combine(pathToRoot(), "mace.min.js");
             min = System.Text.RegularExpressions.Regex.Replace(min, pattern, "");
-           
             System.IO.File.WriteAllText(newFilePath, min);
-            var exists = existsAndIsNew(newFilePath);
-            Assert.IsTrue(exists);
+            Assert.IsTrue(System.IO.File.Exists(jsFile) && System.IO.File.Exists(tsFile) && System.IO.File.Exists(newFilePath));
         }
 
-        void writeMaceJsFile()
+        string writeBundle(string extension)
         {
-            var typeScriptDirectories = new List<string>() {
+            var javascriptDirectories = new List<string>() {
                 "Classes", "Interfaces", "Modules", "Prototypes"
             };
             var sb = new System.Text.StringBuilder();
-            var path = pathToMaceWeb();
-            var newFilePath = Path.Combine(path, "Scripts", "mace.js");
-            foreach (var dir in typeScriptDirectories)
+            var root = pathToRoot();
+            var newFilePath = Path.Combine(root, "mace" + extension);
+            foreach (var dir in javascriptDirectories)
             {
-                var dirToBundle = Path.Combine(path, dir);
-                var files = System.IO.Directory.GetFiles(dirToBundle, "*.js");
+                var dirToBundle = Path.Combine(pathToMaceWeb(), dir);
+                var files = System.IO.Directory.GetFiles(dirToBundle, "*" + extension);
                 foreach (var file in files)
                 {
                     sb.AppendLine(System.IO.File.ReadAllText(file));
                 }
             }
             System.IO.File.WriteAllText(newFilePath, sb.ToString());
+            return newFilePath;
+        }
+
+        string pathToRoot()
+        {
+            var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Replace("file:\\", "");
+            path = path.Substring(0, path.LastIndexOf(@"\mace\") + 5);
+            return path;
         }
 
         bool existsAndIsNew(string file) {
